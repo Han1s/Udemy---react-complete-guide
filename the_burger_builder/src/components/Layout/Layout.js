a20134349c4e7785fbf4fa3fd8907faac1f338d6
Jan Ranostaj <HonzaRanostaj@gmail.com>
2020-12-30 19:06:24 +0100

    143

--- the_burger_builder/src/components/Layout/Layout.js
+++ the_burger_builder/src/components/Layout/Layout.js
@@ -1,8 +1,10 @@
 import React, { Fragment } from 'react';
 import classes from './Layout.module.css';
+import Toolbar from '../Navitation/Toolbar/Toolbar';
 
 const layout = (props) => (
     <Fragment>
+        <Toolbar />
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
