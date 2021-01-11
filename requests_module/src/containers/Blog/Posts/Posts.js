6a4cba3acdba6c3226dcf867e7af62eacabcd312
Jan Ranostaj <HonzaRanostaj@gmail.com>
2021-01-11 22:40:31 +0100

    206

--- requests_module/src/containers/Blog/Posts/Posts.js
+++ requests_module/src/containers/Blog/Posts/Posts.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
 import Post from '../../../components/Post/Post';
-import { Link } from 'react-router-dom';
 
 
 class Posts extends Component {
@@ -29,7 +28,7 @@ class Posts extends Component {
       }
 
     postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id})
+        this.props.history.push({pathname: '/' + id});
     }
 
     render () {
@@ -37,12 +36,13 @@ class Posts extends Component {
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
                 return (
-                    <Link key={post.id} to={'/' + post.id}>
+                    // <Link key={post.id} to={'/' + post.id}>
                         <Post 
-                        title={post.title} 
-                        author={post.author} 
-                        clicked={() => this.postSelectedHandler(post.id)}/>
-                    </Link>
+                            key={post.id}
+                            title={post.title} 
+                            author={post.author} 
+                            clicked={() => this.postSelectedHandler(post.id)}/>
+                    // </Link>
                 )
             })
         }
