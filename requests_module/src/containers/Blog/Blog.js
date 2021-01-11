8410ecc244ce11b819818514d904a720bb951bc2
Jan Ranostaj <HonzaRanostaj@gmail.com>
2021-01-11 22:15:43 +0100

    201

--- requests_module/src/containers/Blog/Blog.js
+++ requests_module/src/containers/Blog/Blog.js
@@ -3,7 +3,7 @@ import axios from '../../axios';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
@@ -13,12 +13,19 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                            <li><NavLink 
+                                to="/" 
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline',
+                                }}>Home</NavLink></li>
+                            <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true',
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
