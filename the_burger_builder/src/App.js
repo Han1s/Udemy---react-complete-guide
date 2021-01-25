bb5cee538b0dc2970af719f81b8a3fdd1db355d9
Jan Ranostaj <HonzaRanostaj@gmail.com>
2021-01-25 22:34:51 +0100

    222

--- the_burger_builder/src/App.js
+++ the_burger_builder/src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
-  state = {
-    show: true,
-  };
 
   render() {
     return (
       <div>
         <Layout>
-          {this.state.show? <BurgerBuilder /> : null}
-          <Checkout />
+            <Switch>
+              <Route path="/checkout" component={Checkout} />
+              <Route path="/" component={BurgerBuilder} />
+            </Switch>
         </Layout>
       </div>
     )
