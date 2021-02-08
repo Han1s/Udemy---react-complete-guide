605e4fe9b359c693a50796563bcf7d8681114111
Han1s <honzaranostaj@gmail.com>
2021-02-08 21:13:14 +0100

    243

--- the_burger_builder/src/containers/Checkout/ContactData/ContactData.js
+++ the_burger_builder/src/containers/Checkout/ContactData/ContactData.js
@@ -42,6 +42,8 @@ class ContactData extends Component {
                 value: '',
                 validation: {
                     required: true,
+                    minLength: 5,
+                    maxLength: 5,
                 },
                 valid: false
             },
@@ -111,14 +113,18 @@ class ContactData extends Component {
     }
 
     checkValidity(value, rules) {
-        let isValid = false;
+        let isValid = true;
         
         if (rules.required) {
-            isValid = value.trim() !== '';
+            isValid = value.trim() !== '' && isValid;
         }
 
         if (rules.minLength) {
-            isValid = value.length >= rules.minLength;
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
         }
 
         return isValid;
