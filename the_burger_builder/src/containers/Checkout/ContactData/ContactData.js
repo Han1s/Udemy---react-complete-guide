30f88dea80f49ff5a703ea1b123c51571576742c
Han1s <honzaranostaj@gmail.com>
2021-02-08 21:41:18 +0100

    247

--- the_burger_builder/src/containers/Checkout/ContactData/ContactData.js
+++ the_burger_builder/src/containers/Checkout/ContactData/ContactData.js
@@ -85,8 +85,10 @@ class ContactData extends Component {
                     ]
                 },
                 value: '',
+                valid: true
             },
-        },  
+        },
+        formIsValid: false,
         loading: false,
     }
 
@@ -143,8 +145,12 @@ class ContactData extends Component {
         updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
         updatedFormElement.touched = true;
         updatedOrderForm[inputIdentifier] = updatedFormElement;
-        console.log(updatedFormElement);
-        this.setState({orderForm: updatedOrderForm});
+
+        let formIsValid = true;
+        for (let inputIdentifier in updatedOrderForm) {
+            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+        }
+        this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
     }
 
     render () {
@@ -169,7 +175,7 @@ class ContactData extends Component {
                         touched={formElement.config.touched}
                         shouldValidate={formElement.config.validation}/>
                 ))}
-                <Button btnType="Success">
+                <Button btnType="Success" disabled={!this.state.formIsValid}>
                     Order
                 </Button>
             </form>
