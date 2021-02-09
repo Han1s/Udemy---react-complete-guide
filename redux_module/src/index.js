828611715f82154c6d1eba81075f631a6c78b2b9
Han1s <honzaranostaj@gmail.com>
2021-02-09 20:37:46 +0100

    258

--- redux_module/src/index.js
+++ redux_module/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './store/reducer';
+
+import { createStore } from 'redux';
+
+const store = createStore(reducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
